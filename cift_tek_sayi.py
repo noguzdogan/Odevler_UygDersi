0d03667ead72fdfe2c25d2cdf7c9e99c3e822d93
noguzdogan <72796743+noguzdogan@users.noreply.github.com>
2020-10-22 15:44:51 +0300

    Add files via upload

---
+++ cift_tek_sayi.py
@@ -0,0 +1,5 @@
+sayi = int(input("Bir sayı giriniz: "))
+if (sayi % 2 == 0):
+    print("Girdiğiniz "," sayısı bir çift sayıdır.",sep=str(sayi))
+else:
+    print("Girdiğiniz "," sayısı bir tek sayıdır.",sep=str(sayi))
