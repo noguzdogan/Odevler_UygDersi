b9dd15ba0743bda2aed56e2195642a0e0dbcd14e
Naip Oğuz Doğan <72796743+noguzdogan@users.noreply.github.com>
2020-10-26 21:47:53 +0300

    Delete Alistirma5.py

--- Alistirma5.py
+++
@@ -1,6 +0,0 @@
-print("4 basamaklı palindromik sayılar şunlardır:\n")
-for x in range(1000,9999):
-    strduz = str(x)
-    strters = strduz[::-1]
-    if (strduz == strters):
-        print(strduz,end=" ")
