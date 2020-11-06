f6bb811acba002284135649eb2ebbe7885b07f46
Naip Oğuz Doğan <72796743+noguzdogan@users.noreply.github.com>
2020-11-06 21:55:15 +0300

    Update Dört_haneli_parola.py

--- Dört_haneli_parola.py
+++ Dört_haneli_parola.py
@@ -1,6 +1,6 @@
 sifre = input("Lütfen yeni bir şifre giriniz: ")   
 dogruluk = 0 # Girilen her hanenin bir rakam olup olmadığını kontrol edicek.
-for x in range(0,len(sifre)): # Sadece ilk 4 hanenin rakam olup olmadığına bakıyorum çünkü while'da şart olarak len(sifre)'yi de belirttim.
+for x in range(0,len(sifre)): # 4 haneden az girilme ihtimaline karşın aralığı len(sifre)'yle belirtiyorum
     if sifre[x].isdigit() == True:
         dogruluk+=1 # Girilen hane bir rakamsa o hane şifre elemanı olmak için doğru bir karakter oluyor.
 while len(sifre) != 4 or dogruluk != 4: # İstenmeyen durumu kontrol ediyor.
