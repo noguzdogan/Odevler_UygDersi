f484416c0784678872f32ab72be661e6ac56de94
Naip Oğuz Doğan <72796743+noguzdogan@users.noreply.github.com>
2020-11-06 14:36:43 +0300

    Update Dört_haneli_parola.py

--- Dört_haneli_parola.py
+++ Dört_haneli_parola.py
@@ -1,5 +1,16 @@
-sifre = input("Lutfen yeni bir sifre giriniz: ")
-while len(sifre) != 4:
-    sifre=input("Girdiginiz sifre 4 haneli degil.\nLutfen tekrar giriniz: ")
-print("Yeni sifreniz: ",sifre)
-# bunu değiştiricem
+sifre = input("Lütfen yeni bir şifre giriniz: ")
+liste = []             
+dogruluk = 0 # Girilen her hanenin bir rakam olup olmadığını kontrol edicek.
+for x in range(0,4): # Sadece ilk 4 hanenin rakam olup olmadığına bakıyorum çünkü while'da şart olarak len(sifre)'yi de belirttim.
+    liste.append(sifre[x])
+    if sifre[x].isdigit() == True:
+        dogruluk+=1 # Girilen hane bir rakamsa o hane şifre elemanı olmak için doğru bir karakter oluyor.
+while len(sifre) != 4 or dogruluk != 4: # İstenmeyen durumu kontrol ediyor.
+    sifre = input("\nGirdiğiniz şifre istenmeyen karakterler barındırıyor.\nLütfen sadece rakamlardan oluşan 4 haneli bir şifre giriniz: ")
+    liste = []
+    dogruluk = 0
+    for x in range(0,4): #Yukarıdaki döngünün aynısı (rekülsif fonksiyona da ekleyebilirdim ama sanırım henüz işlemedik.)
+        liste.append(sifre[x])
+        if sifre[x].isdigit() == True:
+            dogruluk+=1 
+print("Yeni şifreniz: ",sifre)
