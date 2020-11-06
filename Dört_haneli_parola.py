ca83b116c69dbf6df0eb5ba15e53ff195ede0f2f
Naip Oğuz Doğan <72796743+noguzdogan@users.noreply.github.com>
2020-11-06 13:36:11 +0300

    Update Dört_haneli_parola.py

--- Dört_haneli_parola.py
+++ Dört_haneli_parola.py
@@ -2,3 +2,4 @@ sifre = input("Lutfen yeni bir sifre giriniz: ")
 while len(sifre) != 4:
     sifre=input("Girdiginiz sifre 4 haneli degil.\nLutfen tekrar giriniz: ")
 print("Yeni sifreniz: ",sifre)
+# bunu değiştiricem
