15863336f2b68292ef16ad984238e4f9493f92c9
Naip Oğuz Doğan <72796743+noguzdogan@users.noreply.github.com>
2020-11-09 15:31:26 +0300

    Update Dört_haneli_parola.py

--- Dört_haneli_parola.py
+++ Dört_haneli_parola.py
@@ -1,3 +1,4 @@
+# Bu program başlangıçta kullanıcıdan bir girdi ister ve bu girdinin uygun olup olmadığını tek bir while döngüsü içinde kontrol eder.
 sifre = input("Lütfen yeni bir şifre giriniz: ")
 while len(sifre) != 4 or sifre.isdigit() != True: # İstenmeyen durumu kontrol ediyor.
     sifre = input("\nGirdiğiniz şifre istenmeyen karakterler barındırıyor.\nLütfen sadece rakamlardan oluşan 4 haneli bir şifre giriniz: ")
